d2a045ee90d405a05c4c5554a59df5fa672dbfe0
Christopher Bailey <cbailey@mort.is>
2022-05-02 17:42:26 -0400

    Tweaks output

--- src/programs/ghuupdate.lua
+++ src/programs/ghuupdate.lua
@@ -1,7 +1,14 @@
 local ghu = require(settings.get("ghu.base") .. "core/apis/ghu")
 
 print("Updating repos...")
-ghu.updateRepo(ghu.s.coreRepo.get(), true)
+local repoCount = 1
+local count = ghu.updateRepo(ghu.s.coreRepo.get(), true)
 for _, repoString in pairs(ghu.s.extraRepos.get()) do
-    ghu.updateRepo(repoString)
+    count = count + ghu.updateRepo(repoString)
+    repoCount = repoCount + 1
 end
+print(string.format(
+    "Updated %s file%s from %s repo%s",
+    count, count ~= 1 and "s" or "",
+    repoCount, repoCount ~= 1 and "s" or ""
+))
