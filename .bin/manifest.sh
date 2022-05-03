1abb9a5575e5c663891399acdc75bf4cbc85d266
Christopher Bailey <cbailey@mort.is>
2022-05-03 03:14:19 -0400

    Adds minified lua files

--- .bin/manifest.sh
+++ .bin/manifest.sh
@@ -5,9 +5,13 @@ if [[ -f deps.json ]]; then
     DEPS=$(jq -c < deps.json)
 fi
 
-find . -iname "*.lua" \
+LUA_FILES=$(find . -iname "*.lua" ! -iname '*.min.lua')
+echo $LUA_FILES \
     | sed 's/.\///' \
     | xargs sha256sum \
     | awk '{ print "{\""$2"\": \""$1"\"}" }' \
     | jq -s add \
     | jq --argjson dependencies $DEPS '{files: ., dependencies: $dependencies}' > manifest.json
+for file in $LUA_FILES; do
+    luamin -f $file > ${file%.lua}.min.lua
+done
